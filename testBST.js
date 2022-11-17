0c99561baa915fe150c6e9d2a354c41df9e3f08b
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2022-11-17 16:15:30 -0500

    genereate random array to create random a bst, noticed an error on some trees being genearted right sub tree was less than root and left sub tree was greater than root

--- testBST.js
+++ testBST.js
@@ -16,3 +16,12 @@
     8.Print out all elements in level, pre, post, and in order
  */
 import * as bst from './balancedBst.js';
+
+let randomNumberArray = Array.from(Array(Math.floor(Math.random()*9)).keys(),x=>Math.round(Math.random()*100));
+console.log(randomNumberArray,'random size array max length of 9 and random values of up to 100');
+let randomTree = bst.TreeFactory(randomNumberArray);
+if(randomTree.root){
+    bst.prettyPrint(randomTree.root);
+} else {
+    console.log('empty tree');
+}
