4bc9a57abdb943deb69be905cdea12f152e40b4d
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2022-11-18 16:47:25 -0500

    fixed undefined values showing up in the tree just need to remove duplicates using a set before passing to the array to the TreeFactory function

--- testBST.js
+++ testBST.js
@@ -17,8 +17,13 @@
  */
 import * as bst from './balancedBst.js';
 
-let randomNumberArray = Array.from(Array(Math.floor(Math.random()*9)).keys(),x=>Math.round(Math.random()*100));
+//random array from 0 to 9 elements 
+//value 0 to 100 or maybe up to just 99... whatever
+//using set constructor to remove duplicate values or
+//end up with undefined nodes
+let randomNumberArray = [... new Set(Array.from(Array(Math.floor(Math.random()*9)).keys(),x=>Math.round(Math.random()*100)))];
 console.log(randomNumberArray,'random size array max length of 9 and random values of up to 100');
+
 let randomTree = bst.TreeFactory(randomNumberArray);
 if(randomTree.root){
     bst.prettyPrint(randomTree.root);
